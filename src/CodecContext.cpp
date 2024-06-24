5417fbc5699f1a0c213731ba5d8825224442c1a5
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-06-24 23:19:49 +0100

    Allow for opening and sending and recieving, packets and frames

--- src/CodecContext.cpp
+++ src/CodecContext.cpp
@@ -2,19 +2,36 @@
 
 #include "../include/CodecContext.hpp"
 
-
 CodecContext::CodecContext(const AVCodec* codec) {
-    this->codecContext = avcodec_alloc_context3(codec);
-    if (!this->codecContext) {
-        std::cerr << "ERROR: Could not allocate context" << std::endl;
+    AVCodecContext* tempCtx = avcodec_alloc_context3(codec);
+    if (!tempCtx) {
+        std::cerr << "ERROR: AVCodecContext could not be allocated" << std::endl;
         return;
     }
+    std::memcpy(static_cast<AVCodecContext*>(tempCtx), tempCtx, sizeof(AVCodecContext));
+    avcodec_free_context(&tempCtx);
+}
+
+int CodecContext::open(AVDictionary **options) {
+    return avcodec_open2(this, this->codec, options);
+}
+
+int CodecContext::sendFrame(Frame* frame) {
+    return avcodec_send_frame(this, (AVFrame*) frame);
+}
+
+int CodecContext::recieveFrame(Frame* frame) {
+    return avcodec_receive_frame(this, (AVFrame*) frame);
+}
+
+int CodecContext::sendPacket(Packet* packet) {
+    return avcodec_send_packet(this, (AVPacket*) packet);
+}
+
+int CodecContext::recievePacket(Packet* packet) {
+    return avcodec_receive_packet(this, (AVPacket*) packet);
 }
 
 CodecContext::~CodecContext() {
-    avcodec_free_context(&this->codecContext);
-}
-
-AVCodecContext* CodecContext::operator->() {
-    return this->codecContext;
+    avcodec_free_context(reinterpret_cast<AVCodecContext**>(this));
 }

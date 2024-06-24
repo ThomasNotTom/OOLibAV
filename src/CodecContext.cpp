1bc8d6314913892e60b16bbfeaf2fabf2f1f5505
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-06-24 13:03:06 +0100

    Add error catching

--- src/CodecContext.cpp
+++ src/CodecContext.cpp
@@ -1,13 +1,20 @@
-extern "C" {
-    #include <libavcodec/avcodec.h>
-}
+#include <iostream>
 
 #include "../include/CodecContext.hpp"
 
+
 CodecContext::CodecContext(const AVCodec* codec) {
     this->codecContext = avcodec_alloc_context3(codec);
+    if (!this->codecContext) {
+        std::cerr << "ERROR: Could not allocate context" << std::endl;
+        return;
+    }
+}
+
+CodecContext::~CodecContext() {
+    avcodec_free_context(&this->codecContext);
 }
 
 AVCodecContext* CodecContext::operator->() {
-    return codecContext;
+    return this->codecContext;
 }
